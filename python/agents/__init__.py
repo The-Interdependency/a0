2fa9035a03d34dde89a8a9b102e9ce21e6587fa8
Erin Spencer <erin.eps.hovel@gmail.com>
2026-08-16 21:26:53 -0700

    refactor(agents): export PlatonicAgent object

--- python/agents/__init__.py
+++ python/agents/__init__.py
@@ -1,5 +1,5 @@
 # 3:0 0:0 0:2
-from .platonic import AgentDimension, AgentProjection, PlatonicAgentEnvelope, candidate_platonic_agent
+from .platonic import AgentDimension, AgentProjection, PlatonicAgent, candidate_platonic_agent
 from .zfae import ZFAE_AGENT_DEF, compose_name
-__all__ = ["AgentDimension", "AgentProjection", "PlatonicAgentEnvelope", "candidate_platonic_agent", "ZFAE_AGENT_DEF", "compose_name"]
+__all__ = ["AgentDimension", "AgentProjection", "PlatonicAgent", "candidate_platonic_agent", "ZFAE_AGENT_DEF", "compose_name"]
 # 3:0 0:0 0:2
